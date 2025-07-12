b61f900f334daac7882a59db8480c6be946f5abf
Basu <basudevkumar.india2019@gmail.com>
2025-07-12 21:38:41 +0530

    index fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,24 @@ import Header from "./components/Header/Header"
 import Theme from "./components/Theme/Theme"
 import Home from "./page/Home"
 import "animate.css"
-import WOW from "wowjs/dist/wow";
+// import WOW from "wowjs/dist/wow";
 import { ThemeContextProvider } from "./contexts/ThemeContext"
 import { motion } from "framer-motion"
 
 
 function App() {
   useEffect(() => {
-    new WOW().init();
     // new WOW().init();
-    // const WOW = require("wowjs"); 
     // new WOW.WOW().init(); 
+    if (window.WOW) {
+      new window.WOW({
+        boxClass: 'wow',
+        animateClass: 'animate__animated',
+        offset: 0,
+        mobile: true,
+        live: true
+      }).init();
+    }
   }, []);
 
   return (
